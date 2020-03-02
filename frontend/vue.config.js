edd4a15e23927bf448a178fd7658b110d72abb7b
김기찬 <kgichan12@gmail.com>
2020-03-02 15:02:37 +0900

    gitignore 추가 express - backend vue - frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,21 @@
+'use strict'
+var path = require('path');
+
+module.exports = {
+  outputDir: path.resolve(__dirname, '../backend/public'),
+  devServer: {
+    proxy: { // proxyTable 설정
+      '/api': {
+        target: 'http://localhost:3000/api',
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
+        }
+      }
+    }
+  },
+
+  productionSourceMap: false,
+  lintOnSave: false
+}
